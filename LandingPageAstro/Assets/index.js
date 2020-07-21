580545c29d49c650a00c858cd279f6ba06087d41
RitikUpmanyu <51223572+RitikUpmanyu@users.noreply.github.com>
2020-07-21 22:54:54 +0530

    preloader sample

--- LandingPageAstro/Assets/index.js
+++ LandingPageAstro/Assets/index.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function(){
 	$('.togglebutton').on('click', function(){
 		if($('#line1').width() == 30){
@@ -56,4 +58,78 @@ $(document).ready(function(){
 			'opacity' : 1 - st/700
 		})
 	});
+});
+
+// // makes sure the whole site is loaded
+// jQuery(window).load(function() {
+// 	// will first fade out the loading animation
+// jQuery("#status").fadeOut();
+// 	// will fade out the whole DIV that covers the website.
+// jQuery("#preloader").delay(1000).fadeOut("slow");
+// })
+function fadeIn( elem, ms )
+{
+  if( ! elem )
+    return;
+
+  elem.style.opacity = 0;
+  elem.style.filter = "alpha(opacity=0)";
+  elem.style.display = "inline-block";
+  elem.style.visibility = "visible";
+
+  if( ms )
+  {
+    var opacity = 0;
+    var timer = setInterval( function() {
+      opacity += 50 / ms;
+      if( opacity >= 1 )
+      {
+        clearInterval(timer);
+        opacity = 1;
+      }
+      elem.style.opacity = opacity;
+      elem.style.filter = "alpha(opacity=" + opacity * 100 + ")";
+    }, 50 );
+  }
+  else
+  {
+    elem.style.opacity = 1;
+    elem.style.filter = "alpha(opacity=1)";
+  }
+}
+
+function fadeOut( elem, ms )
+{
+  if( ! elem )
+    return;
+
+  if( ms )
+  {
+    var opacity = 1;
+    var timer = setInterval( function() {
+      opacity -= 50 / ms;
+      if( opacity <= 0 )
+      {
+        clearInterval(timer);
+        opacity = 0;
+        elem.style.display = "none";
+        elem.style.visibility = "hidden";
+      }
+      elem.style.opacity = opacity;
+      elem.style.filter = "alpha(opacity=" + opacity * 100 + ")";
+    }, 50 );
+  }
+  else
+  {
+    elem.style.opacity = 0;
+    elem.style.filter = "alpha(opacity=0)";
+    elem.style.display = "none";
+    elem.style.visibility = "hidden";
+  }
+}
+$(window).on('load', function () {
+	//fadeOut(document.getElementById('status'),200);
+	//fadeOut(document.getElementById('preloader'),300);
+	jQuery("#status").fadeOut();
+	jQuery("#preloader").delay(1000).fadeOut("slow");
 });
