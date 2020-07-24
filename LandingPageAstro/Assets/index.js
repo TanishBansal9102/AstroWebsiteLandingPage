96476be88be3041f35be003fb18266a437d9d519
RitikUpmanyu <51223572+RitikUpmanyu@users.noreply.github.com>
2020-07-24 20:57:26 +0530

    explore button class

--- LandingPageAstro/Assets/index.js
+++ LandingPageAstro/Assets/index.js
@@ -36,7 +36,7 @@ $(document).ready(function(){
 	var linea = $('#linea');
 	var lineb = $('#lineb');
 	var linec = $('#linec');
-	var explore = $('#myBtn');
+	var explore = $('.myBtn');
 	var navbar = $('.navbar');
 
 	$(window).on('scroll', function() {
@@ -60,73 +60,6 @@ $(document).ready(function(){
 	});
 });
 
-// // makes sure the whole site is loaded
-// jQuery(window).load(function() {
-// 	// will first fade out the loading animation
-// jQuery("#status").fadeOut();
-// 	// will fade out the whole DIV that covers the website.
-// jQuery("#preloader").delay(1000).fadeOut("slow");
-// })
-function fadeIn( elem, ms )
-{
-  if( ! elem )
-    return;
-
-  elem.style.opacity = 0;
-  elem.style.filter = "alpha(opacity=0)";
-  elem.style.display = "inline-block";
-  elem.style.visibility = "visible";
-
-  if( ms )
-  {
-    var opacity = 0;
-    var timer = setInterval( function() {
-      opacity += 50 / ms;
-      if( opacity >= 1 )
-      {
-        clearInterval(timer);
-        opacity = 1;
-      }
-      elem.style.opacity = opacity;
-      elem.style.filter = "alpha(opacity=" + opacity * 100 + ")";
-    }, 50 );
-  }
-  else
-  {
-    elem.style.opacity = 1;
-    elem.style.filter = "alpha(opacity=1)";
-  }
-}
-
-function fadeOut( elem, ms )
-{
-  if( ! elem )
-    return;
-
-  if( ms )
-  {
-    var opacity = 1;
-    var timer = setInterval( function() {
-      opacity -= 50 / ms;
-      if( opacity <= 0 )
-      {
-        clearInterval(timer);
-        opacity = 0;
-        elem.style.display = "none";
-        elem.style.visibility = "hidden";
-      }
-      elem.style.opacity = opacity;
-      elem.style.filter = "alpha(opacity=" + opacity * 100 + ")";
-    }, 50 );
-  }
-  else
-  {
-    elem.style.opacity = 0;
-    elem.style.filter = "alpha(opacity=0)";
-    elem.style.display = "none";
-    elem.style.visibility = "hidden";
-  }
-}
 $(window).on('load', function () {
 	//fadeOut(document.getElementById('status'),200);
 	//fadeOut(document.getElementById('preloader'),300);
